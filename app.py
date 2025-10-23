c1f02e2454c34a6d874c8a87770d89eecb675c41
Pigle2 <47881291+Pigle2@users.noreply.github.com>
2025-10-23 15:47:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World from Railway! by Pigle"
+    return "Hello, World from Railway! by Pigle Wow~ 한글도 잘 보이려나?"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
